3a02d4852b05737a6ab1f05b6c96e8a254053e56
Forgata <gombwadivine@gmail.com>
2026-03-06 11:02:42 +0200

    refactor: add start function and invoked it

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import { preparePayload } from "./core/embedding/generator.js";
 import { recorder } from "./core/profiler/recorder.js";
 import { initialise } from "./utils/initialise.js";
 
-try {
-  initialise();
+async function start() {
+  try {
+    initialise();
+    console.log("Preparing Bit Stream...");
+    const { finalBitStream } = await preparePayload("file.txt", "1234");
 
-  await recorder();
-} catch (error: unknown) {
-  console.error(error);
+    await recorder();
+  } catch (error: unknown) {
+    console.error(error);
+  }
 }
 
 process.on("SIGINT", () => process.exit());
+
+await start();
