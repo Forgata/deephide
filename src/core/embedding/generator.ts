80342e2d0903c9062e57b21eb69ad074906a2aee
Forgata <gombwadivine@gmail.com>
2026-03-06 11:01:23 +0200

    feat: add payload controller to bridge payload functionality in modular approach

---
+++ src/core/embedding/generator.ts
@@ -0,0 +1,32 @@
+import { randomBytes } from "node:crypto";
+import { framePayload } from "./payload/framing.js";
+import { loadFileToUint8 } from "./payload/Uint8FileReader.js";
+import { deriveKey } from "./crypto/keyDerivation.js";
+import { encryptPayload } from "./crypto/aes.js";
+import { packetize } from "./payload/packer.js";
+import { applyFEC } from "./fec/readSolomon.js";
+import { interleave } from "./fec/interleave.js";
+import { serialiseBits } from "./bitstream/serialiser.js";
+import { injectPreamble } from "./bitstream/preamble.js";
+
+export async function preparePayload(filename: string, password: string) {
+  const rawBytes = await loadFileToUint8(filename);
+  const framed = framePayload(rawBytes, filename);
+
+  const salt = randomBytes(16);
+  const key = await deriveKey(password, salt);
+
+  const encrypted = encryptPayload(framed, key);
+
+  const packets = packetize(encrypted, 256);
+
+  const FEC_SHARDS = applyFEC(packets, 6, 3);
+
+  const interleaved = interleave(FEC_SHARDS, 6, 3);
+
+  const payloadBits = serialiseBits(interleaved);
+
+  const finalBitStream = injectPreamble(payloadBits);
+
+  return { finalBitStream, salt };
+}
