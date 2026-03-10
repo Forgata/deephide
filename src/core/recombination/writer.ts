c00c8e865a919750aad6376e8b4dd0f46dbead15
Forgata <gombwadivine@gmail.com>
2026-03-10 23:51:48 +0200

    refactor include int16

--- src/core/recombination/writer.ts
+++ src/core/recombination/writer.ts
@@ -4,6 +4,11 @@ import fs from "node:fs";
 let pcmAccumulator: Int16Array[] = [];
 
 export function collectOutput(pcmFrame: Int16Array): void {
+  // const newBuffer = new Int16Array(pcmAccumulator.length + pcmFrame.length);
+  // newBuffer.set(pcmAccumulator);
+  // newBuffer.set(pcmFrame, pcmAccumulator.length);
+  // pcmAccumulator = newBuffer;
+
   pcmAccumulator.push(pcmFrame.slice());
 }
 
@@ -12,4 +17,5 @@ export function saveWAV(filename: string = "output.wav"): void {
   wav.fromScratch(1, 16000, "16", pcmAccumulator);
   fs.writeFileSync(filename, wav.toBuffer());
   console.log(`\n stealth audio saved: ${filename}`);
+  // pcmAccumulator = new Int16Array(0);
 }
