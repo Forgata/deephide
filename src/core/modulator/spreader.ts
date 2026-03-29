c9b77067d71d2fc1560f72e0c78f3eb6d06a50e1
Forgata <gombwadivine@gmail.com>
2026-03-29 19:43:09 +0200

    add improved jsdoc fix lfsr typo fix small bugs

--- src/core/modulator/spreader.ts
+++ src/core/modulator/spreader.ts
@@ -13,39 +13,46 @@ export class Spreader {
     this.pnSequence = this.generatePN(1024, seed);
   }
 
+  /**
+   * Generates a PN sequence of the given length using the provided seed.
+   * The generated PN sequence is an array of 64-bit signed integers.
+   * The seed value is used to initialize the LFSR.
+   * @param length The length of the PN sequence to be generated
+   * @param seed The seed value to initialize the LFSR
+   * @returns An array of 64-bit signed integers representing the generated PN sequence
+   */
   private generatePN(length: number, seed: number): Int8Array {
     const pn = new Int8Array(length);
-    let lsfr = seed;
+    let lfsr = seed || 0xaec2;
 
     for (let i = 0; i < length; i++) {
-      lsfr ^= lsfr >> 7;
-      lsfr ^= lsfr << 9;
-      lsfr ^= lsfr >> 13;
-
-      pn[i] = (lsfr & 1) === 1 ? 1 : -1;
+      lfsr ^= lfsr << 13;
+      lfsr ^= lfsr >> 17;
+      lfsr ^= lfsr << 5;
+      pn[i] = lfsr & 1 ? 1 : -1;
     }
     return pn;
   }
 
   /**
-   * @returns the next modulation chip for the bit stream
+   * Retrieves the next modulation chip for the given bit stream at the current bit index.
+   * The method will move to the next bit in the bit stream after generating 64 chips.
+   * @param bitstream The bit stream containing the data to be injected
+   * @param bitPtr The current index of the bit stream
+   * @returns The next modulation chip or null if the bit index has reached the end of the bit stream
    */
-
   public getNextChip(
     bitstream: Uint8Array,
     bitPtr: { index: number },
   ): number | null {
     if (bitPtr.index >= bitstream.length) return null;
 
-    if (this.currentBit === 0)
-      this.currentBit = bitstream[bitPtr.index++] === 1 ? 1 : -1;
-
+    const symbol = bitstream[bitPtr.index] === 1 ? 1 : -1;
     const pnChip = this.pnSequence[this.chipIndex % this.pnSequence.length]!;
+    const modulatedChip = symbol * pnChip;
 
-    const modulatedChip = this.currentBit! * pnChip;
     this.chipIndex++;
-
-    if (this.chipIndex > this.SF) {
+    if (this.chipIndex >= this.SF) {
       this.chipIndex = 0;
       bitPtr.index++;
     }
